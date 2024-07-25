5344fec6cc26d63520ddb9bc2d8c6adf155d2e88
ademesojosiah <josiahademeso@gmail.com>
2024-07-25 09:06:58 +0100

    feat: add input vallidation

--- src/route/auth.route.ts
+++ src/route/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authController from "../controller/auth.controller";
+import { loginValidator, signUpValidator } from "../vallidaton/auth.validation";
 
 const router = Router();
 
-router.post("/signup",authController.signUpController);
-router.post("/login",authController.loginController);
+router.post("/signup",signUpValidator, authController.signUpController);
+router.post("/login",loginValidator, authController.loginController);
 
 export default router;
---
+++ src/vallidaton/auth.validation.ts
@@ -0,0 +1,90 @@
+import "express-async-errors";
+import Joi from "joi";
+import { UserInput } from "../model/user.model";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../utils/error/appError";
+
+export async function signUpValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const signup: UserInput = req.body;
+  const schema = Joi.object({
+    fullname: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string()
+      .required()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
+
+    email: Joi.string().email().required(),
+
+    role: Joi.string().required().valid("USER", "ADMIN"),
+  });
+  try {
+    await schema.validateAsync(signup);
+    next();
+  } catch (error: any) {
+    throw new AppError(401, error.message);
+  }
+}
+
+export async function loginValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const login: { email: string; password: string } = req.body;
+  const schema = Joi.object({
+    password: Joi.string()
+      .required()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
+    email: Joi.string().email().required(),
+  });
+
+  try {
+    await schema.validateAsync(login);
+    next();
+  } catch (error: any) {
+    throw new AppError(401, error.message);
+  }
+}
+
+export async function forgetInputValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const login = req.body;
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+  });
+
+  try {
+    await schema.validateAsync(login);
+    next();
+  } catch (error: any) {
+    throw new AppError(401, error.message);
+  }
+}
+
+export async function resetInputValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const login = req.body;
+  const schema = Joi.object({
+    password: Joi.string()
+      .required()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  });
+
+  try {
+    await schema.validateAsync(login);
+    next();
+  } catch (error: any) {
+    throw new AppError(401, error.message);
+  }
+}
